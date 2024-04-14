f36039208a633a2ed96ca94d45f70f38c9ee0e17
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-15 03:28:18 +0600

    updated JSON description length, made it larger

--- src/Pages/EstateDetails/EstateDetails.jsx
+++ src/Pages/EstateDetails/EstateDetails.jsx
@@ -24,12 +24,15 @@ const EstateDetails = () => {
    return (
       <section className="max-w-[1170px] mx-auto w-[90%] md:w-[80%]">
          <h3>Estate details : {id}</h3>
+         {/* top */}
          <div className="w-full relative">
+            {/* image */}
             <img
                className="w-full object-cover object-center"
                src={estate_image_small}
                alt=""
             />
+            {/* information */}
             <div className="text-center lg:text-start relative w-3/4 mx-auto grid space-y-4 md:space-y-0 md:grid-cols-3 border p-8 z-10 bg-[#252525] left-0 right-0 bottom-14">
                {/* 1 */}
                <div className="">
@@ -55,6 +58,28 @@ const EstateDetails = () => {
                   <p className="text-[15px] tracking-[0.25px]">{area}</p>
                </div>
             </div>
+         </div>
+
+         {/* body details */}
+         <div>
+            <span className="text-sm font-light tracking-[5px] uppercase">
+               {segment_name}
+            </span>
+            <h2 className="tracking-[5px] text-[45px] mb-5 uppercase">
+               {estate_title}
+            </h2>
+
+            <p>{description}</p>
+
+            <ul>
+               {facilities.map((facility, index) => {
+                  return (
+                     <li key={index} className="font-didact">
+                        {facility}
+                     </li>
+                  );
+               })}
+            </ul>
          </div>
       </section>
    );
