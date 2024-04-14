f6d1611ef6fd0cbd2469f4861682123289e28dc0
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-14 17:08:38 +0600

    card design done but not fully done, will have to work on spacing as well as full section width percent

--- src/Components/Estate/Estate.jsx
+++ src/Components/Estate/Estate.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+
 const Estate = ({ eachState }) => {
    const {
       estate_title,
@@ -12,30 +16,54 @@ const Estate = ({ eachState }) => {
    } = eachState;
    return (
       <div>
-         <div className="shadow-md bg-gray-900 text-gray-100">
-            <div>
-               <img
-                  src={estate_image_small}
-                  alt=""
-                  className="object-cover object-center w-full h-72 bg-gray-500"
-               />
+         <div className="border border-gray-700 h-full">
+            <div className="relative">
+               <div className="">
+                  <img
+                     src={estate_image_small}
+                     alt=""
+                     className="object-cover object-center w-full h-72"
+                  />
+               </div>
+               <span className="absolute font-light tracking-[1px] px-4 py-2  bg-ourBlack top-0 right-0 uppercase">
+                  {status}
+               </span>
+               <span className="absolute font-light tracking-[1px] px-4 py-2  bg-ourBlack bottom-0 left-0 uppercase">
+                  {segment_name}
+               </span>
+               <span className="absolute font-light tracking-[1px] px-4 py-2 bg-ourBlack bottom-0 right-0 ">
+                  {price}
+               </span>
             </div>
             <div className="flex flex-col justify-between p-6 space-y-8">
-               <div className="space-y-2">
-                  <h2 className="text-3xl font-semibold tracking-wide">
-                     Donec lectus leo
+               <div className="space-y-2 flex-grow">
+                  <h2 className="text-[20px] font-light tracking-[2px] uppercase ">
+                     {estate_title}
                   </h2>
-                  <p className="text-gray-100">
-                     Curabitur luctus erat nunc, sed ullamcorper erat vestibulum
-                     eget.
+                  <p className="flex items-center gap-1">
+                     <FaLocationDot></FaLocationDot> {location}
                   </p>
+                  <p className="flex items-center gap-1">
+                     <FaHome></FaHome> {area}
+                  </p>
+                  <p className="">
+                     {description.length > 200
+                        ? description.slice(0, 200)
+                        : description}
+                  </p>
+                  <ul className="font-didact">
+                     Facilities:
+                     {facilities.map((facility) => (
+                        <li className="list-disc ml-5">{facility}</li>
+                     ))}
+                  </ul>
                </div>
-               <button
-                  type="button"
-                  className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900"
-               >
-                  Read more
-               </button>
+               <Link className="px-6 py-3.5 w-1/2 mx-auto relative group overflow-hidden inline-block border-ourGold bg-ourGold text-white uppercase font-light text-base tracking-[2px] transition-transform duration-300 ease-out transform active:scale-95 text-center">
+                  <span className="absolute top-0 left-0 flex w-0 h-full mb-0 transition-all duration-300 ease-out transform translate-y-0 bg-ourBlack group-hover:w-full opacity-90"></span>
+                  <span className="relative group-hover:text-white">
+                     view details
+                  </span>
+               </Link>
             </div>
          </div>
       </div>
