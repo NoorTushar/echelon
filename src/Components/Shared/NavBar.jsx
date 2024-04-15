6d8f9eb9b4f77da3c37889fdd95d9ed18a170ecf
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-15 08:32:57 +0600

    changed navbar hamburger color

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import LoginButton from "./LoginButton";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const navItems = (
    <>
@@ -18,27 +19,14 @@ const navItems = (
 const NavBar = () => {
    return (
       <div className="navbar bg-ourBlack py-4">
-         <div className="navbar-start">
-            <div className="dropdown">
+         <div className="navbar-start flex items-center">
+            <div className="dropdown ">
                <div
                   tabIndex={0}
                   role="button"
                   className="btn btn-ghost lg:hidden"
                >
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     className="h-5 w-5"
-                     fill="none"
-                     viewBox="0 0 24 24"
-                     stroke="currentColor"
-                  >
-                     <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h8m-8 6h16"
-                     />
-                  </svg>
+                  <GiHamburgerMenu className="text-ourGold text-2xl"></GiHamburgerMenu>
                </div>
                <ul
                   tabIndex={0}
