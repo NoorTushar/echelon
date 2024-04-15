bb464ae508b934fbc7f150313baf0176909f03ff
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-15 16:11:16 +0600

    firebase secured with env local file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyBxbcqFJGE_OEDY6KOg1hTFMJyPnxn79cc",
-   authDomain: "echelon-tushar.firebaseapp.com",
-   projectId: "echelon-tushar",
-   storageBucket: "echelon-tushar.appspot.com",
-   messagingSenderId: "715697985105",
-   appId: "1:715697985105:web:68153124e3791a0b420a70",
+   apiKey: import.meta.env.VITE_APIKEY,
+   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+   projectId: import.meta.env.VITE_PROJECTID,
+   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+   appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
