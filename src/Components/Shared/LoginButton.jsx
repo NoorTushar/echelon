e0a058dba020c2b7ea3c3c9eee10dabbe9ec66be
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-17 22:05:13 +0600

    fixed button text size in small device

--- src/Components/Shared/LoginButton.jsx
+++ src/Components/Shared/LoginButton.jsx
@@ -4,7 +4,7 @@ const LoginButton = () => {
    return (
       <Link
          to={"/login"}
-         className="px-4 py-1.5 md:px-6 md:py-2.5 relative group overflow-hidden inline-block border-ourGold bg-ourGold text-white uppercase font-light text-base tracking-[2px] transition-transform duration-300 ease-out transform active:scale-95"
+         className="px-4 py-1.5 md:px-6 md:py-2.5 relative group overflow-hidden inline-block border-ourGold bg-ourGold text-white uppercase font-light text-[12px] sm:text-base tracking-[2px] transition-transform duration-300 ease-out transform active:scale-95"
       >
          <span className="absolute top-0 left-0 flex w-0 h-full mb-0 transition-all duration-300 ease-out transform translate-y-0 bg-ourBlack group-hover:w-full opacity-90"></span>
          <span className="relative group-hover:text-white">login</span>
